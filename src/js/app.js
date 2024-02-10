b9d0505d29ca84abbdba1c21f0e96a1a47f624d2
Denis M1rxcle <noonebesideu@gmail.com>
2024-02-10 03:59:55 +0200

    innitial commit

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+const inputGears = document.getElementById("inputGears")
+const innerContainer = document.querySelector(".inner")
+const button = document.querySelector(".searchBtn")
+const listGear = document.querySelector(".listGear")
+const api = "https://jsonplaceholder.typicode.com/users"
+const gearsJson = "http://localhost:3000/gears"
+
+async function showUsers() {
+	try {
+		const response = await fetch(gearsJson)
+
+		if (response.ok) {
+			const data = await response.json()
+
+			createList(data)
+		} else {
+			console.log("Error HTTP: " + response.status)
+		}
+	} catch (error) {
+		console.log("Ошибка получения данных: " + error.message)
+	}
+}
+
+function createList(usersData) {
+	usersData.forEach((usersData) => {
+		const list = `
+				
+		
+		<div class="listGear">
+			<div class="nameGear">${usersData.gearsname}</div>
+			<div class="descriptionGear">${usersData.description}</div>
+		</div>
+	
+				
+			`
+		innerContainer.insertAdjacentHTML("beforeEnd", list)
+	})
+}
+
+button.addEventListener("click", function () {
+	if (innerContainer.childElementCount > 0) {
+		innerContainer.innerHTML = ""
+	} else {
+		showUsers()
+	}
+})
